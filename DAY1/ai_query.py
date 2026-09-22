168519fd18bee5afaa56b2fdca5adfda8513c2b6
G SNEHA <snehaguldanor@gmail.com>
2026-09-22 11:23:49 +0530

    DAY1 python script to run ai query

---
+++ DAY1/ai_query.py
@@ -0,0 +1,10 @@
+import ollama
+response = ollama.chat(
+    model = "llama3.2:3b",
+    messages = [
+        {
+            "role": "user",
+            "content": "Explain machine learning in 40 words.pytg"
+        }
+    ]
+)
